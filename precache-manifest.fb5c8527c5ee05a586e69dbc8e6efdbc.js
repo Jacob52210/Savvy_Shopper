e80f7245641285734854967586ac349c9e5bd252
Jacob Flora <jacobflora4@gmail.com>
2020-07-02 14:25:08 -0400

    Updates

---
+++ precache-manifest.fb5c8527c5ee05a586e69dbc8e6efdbc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3cf4c5064cc46ffd0c5c14696f81e8",
+    "url": "/Savvy_Shopper/index.html"
+  },
+  {
+    "revision": "2c9dd58b31d1262a810f",
+    "url": "/Savvy_Shopper/static/css/main.4c9311f0.chunk.css"
+  },
+  {
+    "revision": "f722b37b70c6fd085f2b",
+    "url": "/Savvy_Shopper/static/js/2.30a9c684.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Savvy_Shopper/static/js/2.30a9c684.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c9dd58b31d1262a810f",
+    "url": "/Savvy_Shopper/static/js/main.58a2ae49.chunk.js"
+  },
+  {
+    "revision": "3015097f3bcb0fa73815",
+    "url": "/Savvy_Shopper/static/js/runtime-main.b07b8092.js"
+  },
+  {
+    "revision": "9a823444cbc94c515da59d00fe717b6d",
+    "url": "/Savvy_Shopper/static/media/afternoon.9a823444.jpg"
+  },
+  {
+    "revision": "37cd5ee7272101c87767d2117b907c5a",
+    "url": "/Savvy_Shopper/static/media/morning.37cd5ee7.jpg"
+  },
+  {
+    "revision": "520f861555d16ef920b7461c6573150d",
+    "url": "/Savvy_Shopper/static/media/night.520f8615.jpg"
+  }
+]);
