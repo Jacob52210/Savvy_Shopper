f71bd511d632e8242fb23c907af9c338f509d1b4
Jacob Flora <jacobflora4@gmail.com>
2020-07-10 15:34:16 -0400

    Search API is functional, just need to sanitize user input

--- savvy-shopper/src/components/recipe/recipe.jsx
+++ savvy-shopper/src/components/recipe/recipe.jsx
@@ -8,36 +8,54 @@ const Recipe = () => {
 	const API_KEY = '2e30a51ebccb2f3115c10140b3e38ea0';
 
 	const [recipes, setRecipes] = useState([]);
+	const [search, setSearch] = useState('');
+	const [query, setQuery] = useState('chicken');
 
 	useEffect(() => {
 		getRecipes();
-	}, []);
+	}, [query]);
 
 	const getRecipes = async () => {
-		const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}`);
+		const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${API_KEY}`);
 		const data = await response.json();
 		setRecipes(data.hits);
-		console.log(data.hits);
+	};
+
+	const updateSearch = (e) => {
+		setSearch(e.target.value);
+	};
+
+	const getSearch = (e) => {
+		e.preventDefault();
+		setQuery(search);
+		setSearch('');
 	};
 
 	return (
 		<div className='Recipe'>
-			<form className='search-form'>
-				<input className='search-bar' type='text' />
+			<h1>Recipe Search</h1>
+			<form onSubmit={getSearch} className='search-form'>
+				<input className='search-bar' type='text' value={search} onChange={updateSearch} placeholder='Search' />
 				<button className='search-btn' type='submit'>
-					Search
+					<i className='fas fa-search'></i>
 				</button>
 			</form>
-			{recipes.map((recipe) => (
-				<Recipes
-					key={recipe.recipe.label}
-					title={recipe.recipe.label}
-					calories={recipe.recipe.calories}
-					image={recipe.recipe.image}
-				/>
-			))}
+			<p>Please limit searches to no more than 5 per minute.</p>
+			<div className='search-results'>
+				{recipes.map((recipe) => (
+					<Recipes
+						key={recipe.recipe.image}
+						title={recipe.recipe.label}
+						calories={recipe.recipe.calories}
+						image={recipe.recipe.image}
+						ingredients={recipe.recipe.ingredients}
+					/>
+				))}
+			</div>
 		</div>
 	);
 };
 
 export default Recipe;
+
+// This Recipe Search API was inspired by Dev Ed. YouTube address: https://www.youtube.com/watch?v=U9T6YkEDkMo&t=1519s
--- savvy-shopper/src/components/recipe/recipe2.jsx
+++ savvy-shopper/src/components/recipe/recipe2.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import './recipe.scss';
 
-const Recipes = ({ title, calories, image }) => {
+const Recipes = ({ title, calories, image, ingredients }) => {
 	return (
-		<div>
+		<div className='cards'>
 			<h1>{title}</h1>
-			<p>{calories}</p>
-			<img src={image} alt={title} />
+			<p>{calories} Calories</p>
+			<img className='food-pics' src={image} alt={title} />
+			<ul>
+				{ingredients.map((ingredient, index) => (
+					<li key={index}>{ingredient.text}</li>
+				))}
+			</ul>
 		</div>
 	);
 };
 
 export default Recipes;
+
+// This Recipe Search API was inspired by Dev Ed. YouTube address: https://www.youtube.com/watch?v=U9T6YkEDkMo&t=1519s
