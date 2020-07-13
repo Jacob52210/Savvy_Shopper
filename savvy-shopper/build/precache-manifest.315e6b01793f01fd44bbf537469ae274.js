f45c1a63228a88c1e401b0df3d525b3c2d557889
Jacob Flora <jacobflora4@gmail.com>
2020-07-13 10:31:56 -0400

    changed color theme of recipe search

---
+++ savvy-shopper/build/precache-manifest.315e6b01793f01fd44bbf537469ae274.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f00bdb89b6007bc4eacb05d772c167b",
+    "url": "/Savvy_Shopper/index.html"
+  },
+  {
+    "revision": "bdea85095ef1b2a2517c",
+    "url": "/Savvy_Shopper/static/css/main.bbb75daf.chunk.css"
+  },
+  {
+    "revision": "6ba1a671da375f7b96e3",
+    "url": "/Savvy_Shopper/static/js/2.acd46c62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Savvy_Shopper/static/js/2.acd46c62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdea85095ef1b2a2517c",
+    "url": "/Savvy_Shopper/static/js/main.a9d52b7f.chunk.js"
+  },
+  {
+    "revision": "3015097f3bcb0fa73815",
+    "url": "/Savvy_Shopper/static/js/runtime-main.b07b8092.js"
+  },
+  {
+    "revision": "9a823444cbc94c515da59d00fe717b6d",
+    "url": "/Savvy_Shopper/static/media/afternoon.9a823444.jpg"
+  },
+  {
+    "revision": "37cd5ee7272101c87767d2117b907c5a",
+    "url": "/Savvy_Shopper/static/media/morning.37cd5ee7.jpg"
+  },
+  {
+    "revision": "520f861555d16ef920b7461c6573150d",
+    "url": "/Savvy_Shopper/static/media/night.520f8615.jpg"
+  }
+]);
